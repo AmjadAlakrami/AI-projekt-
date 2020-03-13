84e995966d86cd6ec58a883d0fc70a2aedde7a48
AmjadAlakrami <43879892+AmjadAlakrami@users.noreply.github.com>
2020-03-13 23:24:14 +0100

    s

--- renamer.py
+++ renamer.py
@@ -3,12 +3,18 @@ import os
 def main(): 
     i = 0
     
-    for filename in os.listdir("C:/Users/amjad/OneDrive/Documents/GitHub/AI-projekt-/Images/thumbs_up_–_Google_Sök"): 
-        dst ="0000" + str(i) + ".JPEG"
-        src ='C:/Users/amjad/OneDrive/Documents/GitHub/AI-projekt-/Images/thumbs_up_–_Google_Sök/'+ filename 
-        dst ="C:/Users/amjad/OneDrive/Documents/GitHub/AI-projekt-/Images/thumbs_up_–_Google_Sök/"+ dst 
-        
-        os.rename(src, dst) 
+    for filename in os.listdir("C:/Users/amjad/OneDrive/Documents/GitHub/AI-projekt-/IMPORTENT/annotationsXML_2"):
+        f = filename.split(".") 
+        s = f[0]+".JPEG"
+
+        #dst ="0000" + str(i) + ".JPEG"
+        #src ='C:/Users/amjad/OneDrive/Documents/GitHub/AI-projekt-/IMPORTENT/Images/thumbs_up_–_Google_Sök/'+ s 
+        #dst ="C:/Users/amjad/OneDrive/Documents/GitHub/AI-projekt-/Images/thumbs_up_–_Google_Sök/"+ dst 
+        if s in os.listdir("C:/Users/amjad/OneDrive/Documents/GitHub/AI-projekt-/IMPORTENT/Images/thumbs_up_–_Google_Sök"):
+            print ("finded")
+        else:
+            os.remove("C:/Users/amjad/OneDrive/Documents/GitHub/AI-projekt-/IMPORTENT/annotationsXML_2/"+filename)
+        #os.rename(src, dst) 
         i += 1
  
 main()
