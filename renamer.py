7839300e2f6794d5a722576ee57c4e029f2c4d8b
AmjadAlakrami <43879892+AmjadAlakrami@users.noreply.github.com>
2020-03-13 22:41:56 +0100

    s

--- renamer.py
+++ renamer.py
@@ -1,12 +1,12 @@
 import os 
  
 def main(): 
-    i = 47
+    i = 0
     
-    for filename in os.listdir("C:/Users/s8amjala/Desktop/IMPORTENT/annotationsXML"): 
-        dst ="000" + str(i) + ".xml"
-        src ='C:/Users/s8amjala/Desktop/IMPORTENT/annotationsXML/'+ filename 
-        dst ="C:/Users/s8amjala/Desktop/IMPORTENT/annotationsXML/"+ dst 
+    for filename in os.listdir("C:/Users/amjad/OneDrive/Documents/GitHub/AI-projekt-/Images/thumbs_up_–_Google_Sök"): 
+        dst ="0000" + str(i) + ".JPEG"
+        src ='C:/Users/amjad/OneDrive/Documents/GitHub/AI-projekt-/Images/thumbs_up_–_Google_Sök/'+ filename 
+        dst ="C:/Users/amjad/OneDrive/Documents/GitHub/AI-projekt-/Images/thumbs_up_–_Google_Sök/"+ dst 
         
         os.rename(src, dst) 
         i += 1
