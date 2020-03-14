5be84314cb80046707e36df3f027be4c5a083eb1
AmjadAlakrami <43879892+AmjadAlakrami@users.noreply.github.com>
2020-03-14 19:28:30 +0100

    j

--- image_det/evaluate.py
+++ image_det/evaluate.py
@@ -6,7 +6,7 @@ import spotipy.util as util
 
 DEFAULT_CONFIG_FILE = os.path.join("C:/Users/amjad/OneDrive/Documents/GitHub/AI-projekt-/image_det", "config.json")
 DEFAULT_WEIGHT_FILE = os.path.join("C:/Users/amjad/Downloads", "model.h5")
-DEFAULT_THRESHOLD = 0.55
+DEFAULT_THRESHOLD = 0.5
 
 SPOTIPY_CLIENT_ID='830e5bf515504d4293f128fe0fa77191'
 SPOTIPY_CLIENT_SECRET='abd8c43c229342dbbfff672a04f6d132'
@@ -32,8 +32,8 @@ if __name__ == '__main__':
                        config['model']['anchors'])
     yolo.load_weights(DEFAULT_WEIGHT_FILE )
     a = []
-    for i in range(0, spotifyObject_1.user_playlist_tracks(playlist_id="4QGhaoFzUW8m7qeBur7Kfi")["total"]):
-        a.append(spotifyObject_1.user_playlist_tracks(offset=i, limit=1, playlist_id="4QGhaoFzUW8m7qeBur7Kfi")["items"][0]["track"]["id"])
+    # for i in range(0, spotifyObject_1.user_playlist_tracks(playlist_id="4QGhaoFzUW8m7qeBur7Kfi")["total"]):
+    #     a.append(spotifyObject_1.user_playlist_tracks(offset=i, limit=1, playlist_id="4QGhaoFzUW8m7qeBur7Kfi")["items"][0]["track"]["id"])
 
     cap = cv2.VideoCapture(0)
     while True:
@@ -47,25 +47,25 @@ if __name__ == '__main__':
         cv2.imshow("imageo", frame)
         cv2.waitKey(1)
 
-        if 1 in labels:
-            if spotifyObject_1.currently_playing()["item"]["id"] not in a:
-                a.append(spotifyObject_1.currently_playing()["item"]["id"])
-                spotifyObject_1.user_playlist_add_tracks(user=USERNAME ,playlist_id="4QGhaoFzUW8m7qeBur7Kfi",tracks=[spotifyObject_1.currently_playing()["item"]["id"]])
-            else:
-                print("track is in list")
-        if 0 in labels:
-            for x in boxes:
-                print(x)
-                if x[0] > 150 :
-                    print("1")
-                    if x[0]-260 >= 100:
-                        print("2")
-                        spotifyObject_1.next_track()
-                if x[0] < 90 :
-                    print(3)
-                    if x[0]-260 < -300:
-                        print(4)
-                        spotifyObject_1.previous_track()
+        # if 1 in labels:
+        #     if spotifyObject_1.currently_playing()["item"]["id"] not in a:
+        #         a.append(spotifyObject_1.currently_playing()["item"]["id"])
+        #         spotifyObject_1.user_playlist_add_tracks(user=USERNAME ,playlist_id="4QGhaoFzUW8m7qeBur7Kfi",tracks=[spotifyObject_1.currently_playing()["item"]["id"]])
+        #     else:
+        #         print("track is in list")
+        # if 0 in labels:
+        #     for x in boxes:
+        #         print(x)
+        #         if x[0] > 150 :
+        #             print("1")
+        #             if x[0]-260 >= 100:
+        #                 print("2")
+        #                 spotifyObject_1.next_track()
+        #         if x[0] < 90 :
+        #             print(3)
+        #             if x[0]-260 < -300:
+        #                 print(4)
+        #                 spotifyObject_1.previous_track()
 
     cv2.destroyAllWindows()
             
