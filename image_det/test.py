8a0daf069769c2756b86112fe5ee86a7409e157f
AmjadAlakrami <43879892+AmjadAlakrami@users.noreply.github.com>
2020-04-30 21:02:51 +0200

    Update test.py

--- image_det/test.py
+++ image_det/test.py
@@ -94,19 +94,7 @@ while True:
     if 1 in labels:
         try:
             diff = cv2.absdiff(frame1, frame2)
-            gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-            blur = cv2.GaussianBlur(gray, (5,5), 0)
-            _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
-            dilated = cv2.dilate(thresh, None, iterations=3)
-            contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            for contour in contours:
-                (x, y, w, h) = cv2.boundingRect(contour)
-                if cv2.contourArea(contour) < 10000:
-                    continue
-                cv2.rectangle(frame1, (x,y), (x+w, y+h), (0,255,0), 2)
-                cv2.putText(frame1, "Status: {}".format("Move"), (10, 20), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
-                1, (0,0,255), 3)
-            # cv2.drawContours(frame1, contours, -1, (0,255,0), 2)
+            print(diff)
             Change_song()
         except:
             input(" No active device found ... press ENTER when you are back on track")
